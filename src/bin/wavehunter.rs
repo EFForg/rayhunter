ed2b34a28a8f25f8b0c5c0fd76238c9eb99059a6
Cooper Quintin <cooperq@eff.org>
2023-12-16 18:18:02 -0800

    add timestamps to each pcap file

--- src/bin/wavehunter.rs
+++ src/bin/wavehunter.rs
@@ -4,20 +4,36 @@ use wavehunter::gsmtap_parser::GsmtapParser;
 use wavehunter::pcap::PcapFile;
 
 use log::debug;
+use std::time::SystemTime;
+use std::os::unix::fs;
 
 fn main() -> DiagResult<()> {
     env_logger::init();
 
+    let systime: u64;
+    match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
+        Ok(n) => systime=n.as_secs(),
+        Err(_) => panic!("SystemTime before UNIX EPOCH!"),
+    }
+    let base_path = "/data/wavehunter/";
+    let pcap_path = format!("{}{}-wavehunter.pcap", base_path, systime);
+    let pcap_link = format!("{}wavehunter.pcap", base_path);
+    let debug_path = format!("{}{}-wavehunter.debug", base_path, systime);
+    // we need to remove the file first
+    //fs::symlink(pcap_path.clone(), pcap_link)?;
+
+
     let mut dev = DiagDevice::new()?;
-    dev.enable_debug_mode("/data/wavehunter/wavehunter-debug")?;
+    dev.enable_debug_mode(debug_path)?;
     dev.config_logs()?;
 
     println!("The orca is hunting for stingrays...");
+    println!("Writing pcaps to {}", pcap_path);
 
     let mut gsmtap_parser = GsmtapParser::new();
-    // We are going to want to add a timestamp to this pcap file eventually
-    let mut pcap_file = PcapFile::new("/data/wavehunter/wavehunter.pcap").unwrap();
+    let mut pcap_file = PcapFile::new(pcap_path).unwrap();
     pcap_file.write_iface_header().unwrap();
+
 
     loop {
         for msg in dev.read_response()? {
