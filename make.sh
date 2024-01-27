66342307bc3ec18284eeeb62412b33d625258223
Cooper Quintin <cooperq@eff.org>
2024-01-26 17:01:06 -0800

    update scripts and makefile

--- make.sh
+++ make.sh
@@ -1,2 +1,2 @@
 cargo build
-adb push target/armv7-unknown-linux-gnueabihf/debug/wavehunter /data/wavehunter/wavehunter
+adb push target/armv7-unknown-linux-gnueabihf/debug/rayhunter /data/rayhunter/rayhunter
