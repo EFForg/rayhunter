b6a0ad6bdc1dc1e3c91e7ba3e260f1d99990dcf9
Cooper Quintin <cooperq@eff.org>
2024-01-31 22:00:25 -0800

    fixing bugs in install script

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ cargo build --release
 cd ..
 # Force a switch into the debug mode to enable ADB
 target/x86_64-unknown-linux-gnu/debug/serial AT
-echo -n "device rooted, waiting for reboot"
+echo -n "adb enabled, waiting for reboot"
 until adb shell true 2> /dev/null
 do
     echo -n .
@@ -22,4 +22,13 @@ sleep 1
 target/x86_64-unknown-linux-gnu/debug/serial "AT+SYSCMD=chmod 4755 /bin/rootshell"
 echo "we have root!"
 adb shell /bin/rootshell -c id
+adb shell '/bin/rootshell -c "mkdir /data/rayhunter"'
+adb push config.toml.example /data/rayhunter/config.toml
+adb push scripts/rayhunter_daemon /tmp/rayhunter_daemon
+adb push scripts/misc-daemon /tmp/misc-daemon
+adb shell '/bin/rootshell -c "mv /tmp/rayhunter_daemon /etc/init.d/rayhunter_daemon"'
+adb shell '/bin/rootshell -c "mv /tmp/misc-daemon /etc/init.d/misc-daemon"'
+adb shell '/bin/rootshell -c "chmod 755 /etc/init.d/rayhunter_daemon"'
+adb shell '/bin/rootshell -c "chmod 755 /etc/init.d/misc-daemon"'
 ./make.sh
+adb shell '/bin/rootshell -c "/etc/init.d/rayhunter_daemon start"'
--- make.sh
+++ make.sh
@@ -1,2 +1,3 @@
 cargo build --release
 adb push target/armv7-unknown-linux-gnueabihf/release/rayhunter /data/rayhunter/rayhunter
+adb shell '/bin/rootshell -c "/etc/init.d/rayhunter_daemon restart"'
