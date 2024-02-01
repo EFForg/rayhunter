71d7b90dd9c670106948a61b720a54600e41dbfe
Cooper Quintin <cooperq@eff.org>
2024-01-31 18:18:42 -0800

    update installer

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
+cd serial 
+cargo build_pc
+cd ..
+cd rootshell
 cargo build --release
+cd ..
 # Force a switch into the debug mode to enable ADB
-target/x86_64-unknown-linux-gnu/release/serial AT
+target/x86_64-unknown-linux-gnu/debug/serial AT
 echo -n "device rooted, waiting for reboot"
-until adb shell true
+until adb shell true 2> /dev/null
 do
     echo -n .
     sleep 1
@@ -10,11 +15,11 @@ done
 echo
 echo "it's alive!"
 adb push target/armv7-unknown-linux-gnueabihf/release/rootshell /tmp/
-target/x86_64-unknown-linux-gnu/release/serial "AT+SYSCMD=mv /tmp/rootshell /bin/rootshell"
+target/x86_64-unknown-linux-gnu/debug/serial "AT+SYSCMD=mv /tmp/rootshell /bin/rootshell"
 sleep 1
-target/x86_64-unknown-linux-gnu/release/serial "AT+SYSCMD=chown root /bin/rootshell"
+target/x86_64-unknown-linux-gnu/debug/serial "AT+SYSCMD=chown root /bin/rootshell"
 sleep 1
-target/x86_64-unknown-linux-gnu/release/serial "AT+SYSCMD=chmod 4755 /bin/rootshell"
+target/x86_64-unknown-linux-gnu/debug/serial "AT+SYSCMD=chmod 4755 /bin/rootshell"
 echo "we have root!"
-adb shell id
-adb push target/armv7-unknown-linux-gnueabihf/release/rayhunter /data/rayhunter/rayhunter
+adb shell /bin/rootshell -c id
+./make.sh
