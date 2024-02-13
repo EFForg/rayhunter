d8879c357fa4cf31922a80348de2d875fc0f78b9
Will Greenberg <willg@eff.org>
2024-02-13 13:48:21 -0800

    make.sh: use updated binary name

--- make.sh
+++ make.sh
@@ -1,3 +1,10 @@
+<<<<<<< Updated upstream
 cargo build --release
 adb push target/armv7-unknown-linux-gnueabihf/release/rayhunter /data/rayhunter/rayhunter
 adb shell '/bin/rootshell -c "/etc/init.d/rayhunter_daemon restart"'
+=======
+# the "arm" profile inherits from "release", so this is an optimized build
+cargo build --profile arm
+adb push target/arm/rayhunter-daemon /data/rayhunter/
+adb shell '/bin/rootshell -c "/etc/init.d/rayhunter_daemon restart"'
+>>>>>>> Stashed changes
