a644620eaac7c54862f60f883309f5a8169e859f
Will Greenberg <willg@eff.org>
2024-02-13 16:55:15 -0800

    Build x86-64 by default, fix make script to build for ARM

--- make.sh
+++ make.sh
@@ -1,10 +1,4 @@
-<<<<<<< Updated upstream
-cargo build --release
-adb push target/armv7-unknown-linux-gnueabihf/release/rayhunter /data/rayhunter/rayhunter
+#!/bin/sh
+cargo build --release --target="armv7-unknown-linux-gnueabihf" --bin rayhunter-daemon
+adb push target/armv7-unknown-linux-gnueabihf/release/rayhunter-daemon /data/rayhunter/
 adb shell '/bin/rootshell -c "/etc/init.d/rayhunter_daemon restart"'
-=======
-# the "arm" profile inherits from "release", so this is an optimized build
-cargo build --profile arm
-adb push target/arm/rayhunter-daemon /data/rayhunter/
-adb shell '/bin/rootshell -c "/etc/init.d/rayhunter_daemon restart"'
->>>>>>> Stashed changes
