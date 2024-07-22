aba3a8c3c740b6f07d021ae2b18bbc47df73e92f
Will Greenberg <willg@eff.org>
2024-07-22 12:55:03 -0700

    Release fixes

--- dist/install-common.sh
+++ dist/install-common.sh
@@ -21,7 +21,8 @@ check_adb() {
 
 force_debug_mode() {
     # Force a switch into the debug mode to enable ADB
-    $(SERIAL_PATH) AT
+    echo "$SERIAL_PATH"
+    "$SERIAL_PATH" AT
     echo -n "adb enabled, waiting for reboot"
     until adb shell true 2> /dev/null
     do
@@ -34,11 +35,11 @@ force_debug_mode() {
 
 setup_rootshell() {
     _adb_push rootshell /tmp/
-    $(SERIAL_PATH) "AT+SYSCMD=mv /tmp/rootshell /bin/rootshell"
+    "$SERIAL_PATH" "AT+SYSCMD=cp /tmp/rootshell /bin/rootshell"
     sleep 1
-    $(SERIAL_PATH) "AT+SYSCMD=chown root /bin/rootshell"
+    "$SERIAL_PATH" "AT+SYSCMD=chown root /bin/rootshell"
     sleep 1
-    $(SERIAL_PATH) "AT+SYSCMD=chmod 4755 /bin/rootshell"
+    "$SERIAL_PATH" "AT+SYSCMD=chmod 4755 /bin/rootshell"
     echo "we have root!"
     adb shell /bin/rootshell -c id
 }
@@ -48,13 +49,13 @@ _adb_push() {
 }
 
 setup_rayhunter() {
-    adb shell '/bin/rootshell -c "mkdir /data/rayhunter"'
+    adb shell '/bin/rootshell -c "mkdir -p /data/rayhunter"'
     _adb_push config.toml.example /data/rayhunter/config.toml
     _adb_push rayhunter-daemon /data/rayhunter/
     _adb_push scripts/rayhunter_daemon /tmp/rayhunter_daemon
     _adb_push scripts/misc-daemon /tmp/misc-daemon
-    adb shell '/bin/rootshell -c "mv /tmp/rayhunter_daemon /etc/init.d/rayhunter_daemon"'
-    adb shell '/bin/rootshell -c "mv /tmp/misc-daemon /etc/init.d/misc-daemon"'
+    adb shell '/bin/rootshell -c "cp /tmp/rayhunter_daemon /etc/init.d/rayhunter_daemon"'
+    adb shell '/bin/rootshell -c "cp /tmp/misc-daemon /etc/init.d/misc-daemon"'
     adb shell '/bin/rootshell -c "chmod 755 /etc/init.d/rayhunter_daemon"'
     adb shell '/bin/rootshell -c "chmod 755 /etc/init.d/misc-daemon"'
     adb shell '/bin/rootshell -c "/etc/init.d/rayhunter_daemon start"'
