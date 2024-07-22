0237cb799bd35707e4d3d0c85200b2dfa42849e0
Will Greenberg <willg@eff.org>
2024-07-22 16:48:17 -0700

    rootshell: use magic Android GIDs to access sockets
    Android kernels with CONFIG_ANDROID_PARANOID_NETWORK extensions set
    require users to have a few special group IDs before getting network
    access. Unfortunately, we need to use nightly to get access to the
    .groups() method.

--- rootshell/src/main.rs
+++ rootshell/src/main.rs
@@ -1,9 +1,19 @@
+#![feature(setgroups)]
+
 //! a simple shell for uploading to the orbic device.
 //! 
 //! It literally just runs bash as UID/GID 0 
 use std::process::Command;
 use std::os::unix::process::CommandExt;
 use std::env;
+
+const ANDROID_PARANOID_NETWORK_GROUPS: &[u32] = &[
+   3001, // AID_BT
+   3002, // AID_BT_NET
+   3003, // AID_INET
+   3004, // AID_NET_RAW
+   3005, // AID_ADMIN
+];
 
 fn main() {
    let mut args = env::args();
@@ -14,5 +24,6 @@ fn main() {
 	.args(args)
 	.uid(0)
 	.gid(0)
+   .groups(ANDROID_PARANOID_NETWORK_GROUPS)
 	.exec();
 }
