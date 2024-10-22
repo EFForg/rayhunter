ee753269121ca7791faaed46319e33acc3d83cf3
Cooper Quintin <cooperq@users.noreply.github.com>
2024-10-22 12:21:27 -0700

    Fix macos install (#67)
    * update shell path and some docs
    
    * download ADB if not present
    
    * big O not little o
    
    * bugfix
    
    * bugfix
    
    * silence errors for macos developers
    
    * Update dist/install-common.sh
    
    Co-authored-by: Will Greenberg <willg@eff.org>
    
    ---------
    
    Co-authored-by: Will Greenberg <willg@eff.org>

--- dist/install-linux.sh
+++ dist/install-linux.sh
@@ -1,6 +1,17 @@
 #!/bin/env bash
 
 set -e
+if ! command -v adb &> /dev/null; then
+    if [ ! -d ./platform-tools ] ; then
+        echo "adb not found, downloading local copy"
+        curl -O "https://dl.google.com/android/repository/platform-tools-latest-linux.zip"
+        unzip platform-tools-latest-linux.zip
+    fi
+    export ADB="./platform-tools/adb"
+else
+    export ADB=`which adb`
+fi
+
 export SERIAL_PATH="./serial-ubuntu-latest/serial"
 . "$(dirname "$0")"/install-common.sh
 install
--- dist/install-mac.sh
+++ dist/install-mac.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 
 set -e
+if ! command -v adb &> /dev/null; then
+    if [ ! -d ./platform-tools ]; then
+        echo "adb not found, downloading local copy"
+        curl -O "https://dl.google.com/android/repository/platform-tools-latest-darwin.zip"
+        unzip platform-tools-latest-darwin.zip
+    fi
+    export ADB="./platform-tools/adb"
+else
+    export ADB=`which adb`
+fi
+
 export SERIAL_PATH="./serial-macos-latest/serial"
 . "$(dirname "$0")"/install-common.sh
 install
