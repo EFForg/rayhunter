34d87d1fd7641ae195d7e07bb1f90a630ddc2148
Will Greenberg <willg@eff.org>
2025-01-21 15:11:32 -0800

    this macro isn't public, so docstrings won't work

--- lib/src/analysis/util.rs
+++ lib/src/analysis/util.rs
@@ -1,29 +1,27 @@
 
-/// Unpacks a pattern, or returns None.
-///
-/// # Examples
-/// You can use `unpack!` to unroll highly nested enums like this:
-/// ```
-/// use super::util::unpack;
-///
-/// enum Foo {
-///     A(Bar),
-///     B,
-/// }
-///
-/// enum Bar {
-///     C(Baz)
-/// }
-///
-/// struct Baz;
-///
-/// fn get_bang(foo: Foo) -> Option<Baz> {
-///     unpack!(Foo::A(bar) = foo);
-///     unpack!(Bar::C(baz) = bar);
-///     baz
-/// }
-/// ```
-///
+// Unpacks a pattern, or returns None.
+//
+// # Examples
+// You can use `unpack!` to unroll highly nested enums like this:
+// ```
+// enum Foo {
+//     A(Bar),
+//     B,
+// }
+//
+// enum Bar {
+//     C(Baz)
+// }
+//
+// struct Baz;
+//
+// fn get_bang(foo: Foo) -> Option<Baz> {
+//     unpack!(Foo::A(bar) = foo);
+//     unpack!(Bar::C(baz) = bar);
+//     baz
+// }
+// ```
+//
 macro_rules! unpack {
     ($pat:pat = $val:expr) => {
         let $pat = $val else { return None; };
