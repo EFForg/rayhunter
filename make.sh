f94f292d21bce200083ffba2beb22ae2e96f37ee
Cooper Quintin <cooperq@eff.org>
2025-02-10 11:15:40 -0800

    stop before upload in case file is locked from writing by running process

--- make.sh
+++ make.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 cargo build --release --target="armv7-unknown-linux-gnueabihf" #--features debug
+adb shell '/bin/rootshell -c "/etc/init.d/rayhunter_daemon stop"'
 adb push target/armv7-unknown-linux-gnueabihf/release/rayhunter-daemon /data/rayhunter/rayhunter-daemon
-adb shell '/bin/rootshell -c "/etc/init.d/rayhunter_daemon restart"'
+adb shell '/bin/rootshell -c "/etc/init.d/rayhunter_daemon start"'
