0d9f53f602219055d2eadd76c9408bcaf9272585
Will Greenberg <willg@eff.org>
2025-03-25 16:32:07 -0700

    Update make.sh
    reboot the orbic instead of starting up the process again, since rootshell seems to have insufficient privileges to start rayhunter

--- make.sh
+++ make.sh
@@ -2,4 +2,5 @@
 cargo build --release --target="armv7-unknown-linux-gnueabihf" #--features debug
 adb shell '/bin/rootshell -c "/etc/init.d/rayhunter_daemon stop"'
 adb push target/armv7-unknown-linux-gnueabihf/release/rayhunter-daemon /data/rayhunter/rayhunter-daemon
-adb shell '/bin/rootshell -c "/etc/init.d/rayhunter_daemon start"'
+echo "rebooting the device..."
+adb shell '/bin/rootshell -c "reboot"'
