9ec505d5c419e72d794db920df82b332f2da5ae8
Markus Unterwaditzer <markus-tarpit+git@unterwaditzer.net>
2025-04-02 03:30:18 +0200

    add emoji indicator

--- bin/src/display/tplink.rs
+++ bin/src/display/tplink.rs
@@ -1,11 +1,186 @@
 use crate::config;
 use crate::display::DisplayState;
 
-use log::info;
-use tokio::sync::oneshot;
+use log::{info, error};
 use tokio::sync::mpsc::Receiver;
+use tokio::sync::oneshot;
 use tokio_util::task::TaskTracker;
+use tokio::sync::oneshot::error::TryRecvError;
 
-pub fn update_ui(_task_tracker: &TaskTracker, _config: &config::Config, mut _ui_shutdown_rx: oneshot::Receiver<()>, mut _ui_update_rx: Receiver<DisplayState>) {
-    info!("Spawning dummy UI due to unsupported build.");
+use std::fs;
+use std::thread::sleep;
+use std::time::Duration;
+
+macro_rules! pixel {
+    (x) => { 0 };
+    (_) => { 1 };
+}
+
+macro_rules! pixelart {
+    ($($tt:tt)*) => {{
+        let mut bytes = Vec::new();
+        let mut i = 0;
+        let mut byte = 0;
+        $(
+            byte |= pixel!($tt);
+            if i == 7 {
+                bytes.push(byte);
+                i = 0;
+                byte = 0;
+            } else {
+                i += 1;
+                byte <<= 1;
+            }
+        )*
+
+        // last byte is bogus, discard it to silence warnings
+        let _byte = byte;
+
+        assert_eq!(i % 8, 0);
+        bytes
+    }}
+}
+
+fn paused() -> Vec<u8> {
+    let mut command = vec![
+        104, // x (from left)
+        56,  // y (from top)
+        16,  // width
+        16,  // height
+    ];
+
+    command.extend(pixelart! {
+        _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ 
+        _ _ _ x x x x x x x x x x _ _ _ 
+        _ x x _ _ _ _ _ _ _ _ _ _ x x _ 
+        _ x _ _ _ _ _ _ _ _ _ _ _ _ x _ 
+        _ x _ _ _ _ _ _ _ _ _ _ _ _ x _ 
+        _ x _ _ _ x _ _ _ _ x _ _ _ x _ 
+        _ x _ _ _ _ _ _ _ _ _ _ _ _ x _ 
+        _ x _ _ _ _ _ _ _ _ _ _ _ _ x _ 
+        _ x _ _ _ _ _ _ _ _ _ _ _ _ x _ 
+        _ x _ _ _ _ _ _ _ _ _ _ _ _ x _ 
+        _ x _ _ _ _ _ _ _ _ _ _ _ _ x _ 
+        _ x _ _ _ _ _ _ _ _ _ _ _ _ x _ 
+        _ x _ _ _ _ _ _ _ _ _ _ _ _ x _ 
+        _ x x _ _ _ _ _ _ _ _ _ _ x x _ 
+        _ _ _ x x x x x x x x x x _ _ _ 
+        _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ 
+    });
+    command
+}
+
+fn smiling() -> Vec<u8> {
+    let mut command = vec![
+        104, // x (from left)
+        56,  // y (from top)
+        16,  // width
+        16,  // height
+    ];
+
+    command.extend(pixelart! {
+        _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ 
+        _ _ _ x x x x x x x x x x _ _ _ 
+        _ x x _ _ _ _ _ _ _ _ _ _ x x _ 
+        _ x _ _ _ _ _ _ _ _ _ _ _ _ x _ 
+        _ x _ _ _ _ _ _ _ _ _ _ _ _ x _ 
+        _ x _ _ _ x _ _ _ _ x _ _ _ x _ 
+        _ x _ _ _ _ _ _ _ _ _ _ _ _ x _ 
+        _ x _ _ _ _ _ _ _ _ _ _ _ _ x _ 
+        _ x _ _ _ x _ _ _ _ x _ _ _ x _ 
+        _ x _ _ _ x _ _ _ _ x _ _ _ x _ 
+        _ x _ _ _ x x x x x x _ _ _ x _ 
+        _ x _ _ _ _ _ _ _ _ _ _ _ _ x _ 
+        _ x _ _ _ _ _ _ _ _ _ _ _ _ x _ 
+        _ x x _ _ _ _ _ _ _ _ _ _ x x _ 
+        _ _ _ x x x x x x x x x x _ _ _ 
+        _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ 
+    });
+    command
+}
+
+fn frowning() -> Vec<u8> {
+    let mut command = vec![
+        104, // x (from left)
+        56,  // y (from top)
+        16,  // width
+        16,  // height
+    ];
+    command.extend(
+        pixelart! {
+            _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ 
+            _ _ _ x x x x x x x x x x _ _ _ 
+            _ x x _ _ _ _ _ _ _ _ _ _ x x _ 
+            _ x _ _ _ _ _ _ _ _ _ _ _ _ x _ 
+            _ x _ _ _ _ _ _ _ _ _ _ _ _ x _ 
+            _ x _ _ _ x _ _ _ _ x _ _ _ x _ 
+            _ x _ _ _ _ _ _ _ _ _ _ _ _ x _ 
+            _ x _ _ _ _ _ _ _ _ _ _ _ _ x _ 
+            _ x _ _ _ x x x x x x _ _ _ x _ 
+            _ x _ _ _ x _ _ _ _ x _ _ _ x _ 
+            _ x _ _ _ x _ _ _ _ x _ _ _ x _ 
+            _ x _ _ _ _ _ _ _ _ _ _ _ _ x _ 
+            _ x _ _ _ _ _ _ _ _ _ _ _ _ x _ 
+            _ x x _ _ _ _ _ _ _ _ _ _ x x _ 
+            _ _ _ x x x x x x x x x x _ _ _ 
+            _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ 
+        }
+    );
+    command
+}
+
+pub fn update_ui(
+    task_tracker: &TaskTracker,
+    config: &config::Config,
+    mut ui_shutdown_rx: oneshot::Receiver<()>,
+    mut ui_update_rx: Receiver<DisplayState>,
+) {
+    let display_level = config.ui_level;
+    if display_level == 0 {
+        info!("Invisible mode, not spawning UI.");
+    }
+
+    task_tracker.spawn_blocking(move || {
+        loop {
+            match ui_shutdown_rx.try_recv() {
+                Ok(_) => {
+                    info!("received UI shutdown");
+                    break;
+                },
+                Err(TryRecvError::Empty) => {},
+                Err(e) => panic!("error receiving shutdown message: {e}")
+            }
+
+            let pixels = match ui_update_rx.try_recv() {
+                Ok(DisplayState::Paused) => paused(),
+                Ok(DisplayState::Recording) => smiling(),
+                Ok(DisplayState::RecordingCBM) => smiling(),
+                Ok(DisplayState::WarningDetected) => frowning(),
+                Err(tokio::sync::mpsc::error::TryRecvError::Empty) => {
+                    sleep(Duration::from_millis(1000));
+                    continue
+                },
+                Err(e) => {
+                    error!("error receiving framebuffer update message: {e}");
+                    continue
+                }
+                _ => {
+                    continue
+                }
+            };
+
+            if display_level != 0 {
+                info!("writing to display");
+                if let Err(e) = fs::write("/sys/class/display/oled/oled_buffer", pixels) {
+                    error!("failed to write to display: {e}");
+                }
+            }
+        }
+    });
+}
+
+#[test]
+fn test_pixels() {
+    let pixels = frowning();
+    assert_eq!(pixels, [104, 56, 16, 16, 255, 255, 224, 7, 159, 249, 191, 253, 191, 253, 187, 221, 191, 253, 191, 253, 184, 29, 187, 221, 187, 221, 191, 253, 191, 253, 159, 249, 224, 7, 255, 255]);
 }
