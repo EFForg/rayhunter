2fc01449059157307ea5ffadf758510ce0ca387a
Will Greenberg <willg@eff.org>
2025-04-16 10:57:18 -0700

    update make script to build site

--- make.sh
+++ make.sh
@@ -1,4 +1,7 @@
-#!/bin/sh
+#!/bin/bash -e
+pushd bin/web
+    npm run build
+popd
 cargo build --release --target="armv7-unknown-linux-gnueabihf" #--features debug
 adb shell '/bin/rootshell -c "/etc/init.d/rayhunter_daemon stop"'
 adb push target/armv7-unknown-linux-gnueabihf/release/rayhunter-daemon /data/rayhunter/rayhunter-daemon
