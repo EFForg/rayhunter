50a69147248ba048aeb26f33c0bf1fda5422108e
Caleb <57644769+alliraine@users.noreply.github.com>
2025-05-15 21:56:07 -0400

    Add RayHunter branding colors to TailwindCSS

--- bin/web/tailwind.config.ts
+++ bin/web/tailwind.config.ts
@@ -4,7 +4,13 @@ export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
 
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'rayhunter-blue': '#4e4eb1',
+        'rayhunter-dark-blue': '#3f3da0',
+        'rayhunter-green': '#94ea18'
+      }
+    }
   },
 
   plugins: []
