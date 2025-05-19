ede76aae2e0d80ee96e4c253ea7d5abfd7facce8
Cooper Quintin <cooperq@eff.org>
2025-05-19 16:22:03 -0700

    fix more issues with windows installer

--- docker_make.sh
+++ docker_make.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -e
-pushd bin/web
+cd bin/web
     npm run build
-popd
-#docker build -t rayhunter-devenv -f tools/devenv.dockerfile .
-docker run --user $UID:$GID -v ./:/workdir -w /workdir -it rayhunter-devenv sh -c 'cargo build --release --target="armv7-unknown-linux-gnueabihf"'
+cd ..
+docker build -t rayhunter-devenv -f tools/devenv.dockerfile .
+echo ' build!'
+docker run --user $UID:$GID -v ./:/workdir -w /workdir -it rayhunter-devenv sh -c 'cargo build --release --target="armv7-unknown-linux-musleabihf"'
 adb shell '/bin/rootshell -c "/etc/init.d/rayhunter_daemon stop"'
-adb push target/armv7-unknown-linux-gnueabihf/release/rayhunter-daemon /data/rayhunter/rayhunter-daemon
+adb push target/armv7-unknown-linux-musleabihf/release/rayhunter-daemon /data/rayhunter/rayhunter-daemon
 echo "rebooting the device..."
 adb shell '/bin/rootshell -c "reboot"'
