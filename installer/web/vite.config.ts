4cb613280b5c0e252e0223861d761ed1055dbc8d
Caleb <57644769+alliraine@users.noreply.github.com>
2025-05-23 13:46:21 -0400

    Remove boilerplate

--- installer/web/vite.config.ts
+++ installer/web/vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import tailwindcss from '@tailwindcss/vite'
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    svelte(),
+    tailwindcss()
+  ],
 })
