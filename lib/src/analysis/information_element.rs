e3208748549c2163977051aecf3bb89a62a7c3d1
Markus Unterwaditzer <markus-tarpit+git@unterwaditzer.net>
2025-06-19 23:17:53 +0200

    Remove PartialEq, Clone, Serialize from LTE RRC parser
    Compiling telecom-parser takes 2 minutes on my machine. After removing
    those derives it takes 1:15. I suspect it's mostly serde though.

--- lib/src/analysis/information_element.rs
+++ lib/src/analysis/information_element.rs
@@ -18,7 +18,7 @@ pub enum InformationElementError {
     UnsupportedGsmtapType(GsmtapType),
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 pub enum InformationElement {
     GSM,
     UMTS,
@@ -28,7 +28,7 @@ pub enum InformationElement {
     FiveG,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 pub enum LteInformationElement {
     DlCcch(lte_rrc::DL_CCCH_Message),
     // This element of the enum is substantially larger than the others,
