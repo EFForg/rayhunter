1a2e8e4a0db33d39088c614407d8c4e5ffa91bdb
Cooper Quintin <cooperq@eff.org>
2025-06-24 17:19:53 -0700

    cargo fmt

--- lib/src/analysis/mod.rs
+++ lib/src/analysis/mod.rs
@@ -3,7 +3,7 @@ pub mod connection_redirect_downgrade;
 pub mod imsi_provided;
 pub mod imsi_requested;
 pub mod information_element;
-pub mod null_cipher;
 pub mod nas_null_cipher;
+pub mod null_cipher;
 pub mod priority_2g_downgrade;
 pub mod util;
