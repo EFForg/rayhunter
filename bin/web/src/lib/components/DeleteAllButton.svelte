5e328b889bcb27b2aff9607109f558553f2eebea
Markus Unterwaditzer <markus-tarpit+git@unterwaditzer.net>
2025-06-28 20:42:23 +0200

    Fix eslint issues

--- bin/web/src/lib/components/DeleteAllButton.svelte
+++ bin/web/src/lib/components/DeleteAllButton.svelte
@@ -1,12 +1,5 @@
 <script lang="ts">
-    import { req } from '$lib/utils.svelte';
     import DeleteButton from './DeleteButton.svelte';
-
-    function confirmDelete() {
-        if (window.confirm(`Permanently delete ALL recordings?`)) {
-            req('POST', '/api/delete-all-recordings');
-        }
-    }
 </script>
 
 <div class="flex flex-row justify-end gap-2">
