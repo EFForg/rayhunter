7302dfffbf0b4ff1c7e6893ce4b8cd49c30d19b3
Sashanoraa <sasha@noraa.gay>
2025-07-28 14:08:20 -0400

    Use firmware-devel profile for make.sh

--- make.sh
+++ make.sh
@@ -2,8 +2,9 @@
 pushd daemon/web
     npm run build
 popd
-cargo build --profile firmware --bin rayhunter-daemon --target="armv7-unknown-linux-musleabihf" #--features debug
+cargo build --profile firmware-devel --bin rayhunter-daemon --target="armv7-unknown-linux-musleabihf" #--features debug
 adb shell '/bin/rootshell -c "/etc/init.d/rayhunter_daemon stop"'
-adb push target/armv7-unknown-linux-musleabihf/firmware/rayhunter-daemon /data/rayhunter/rayhunter-daemon
+adb push target/armv7-unknown-linux-musleabihf/firmware-devel/rayhunter-daemon \
+    /data/rayhunter/rayhunter-daemon
 echo "rebooting the device..."
 adb shell '/bin/rootshell -c "reboot"'
