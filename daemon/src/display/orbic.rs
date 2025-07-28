c2226071d24c819e39e7b401f60fe7c68969c4c9
Sashanoraa <sasha@noraa.gay>
2025-07-28 17:46:42 -0400

    Dynamically determine frame buffer dimensions and format
    This commit also refactors a majority of the device specific frame
    buffer code into common code into generic_framebuffer.

--- daemon/src/display/orbic.rs
+++ daemon/src/display/orbic.rs
@@ -1,39 +1,14 @@
 use crate::config;
 use crate::display::DisplayState;
-use crate::display::generic_framebuffer::{self, Dimensions, GenericFramebuffer};
-use async_trait::async_trait;
+use crate::display::generic_framebuffer;
 
 use tokio::sync::mpsc::Receiver;
 use tokio::sync::oneshot;
 use tokio_util::task::TaskTracker;
 
+use super::generic_framebuffer::FramebufferDevice;
+
 const FB_PATH: &str = "/dev/fb0";
-
-#[derive(Copy, Clone, Default)]
-struct Framebuffer;
-
-#[async_trait]
-impl GenericFramebuffer for Framebuffer {
-    fn dimensions(&self) -> Dimensions {
-        // TODO actually poll for this, maybe w/ fbset?
-        Dimensions {
-            height: 128,
-            width: 128,
-        }
-    }
-
-    async fn write_buffer(&mut self, buffer: Vec<(u8, u8, u8)>) {
-        let mut raw_buffer = Vec::new();
-        for (r, g, b) in buffer {
-            let mut rgb565: u16 = (r as u16 & 0b11111000) << 8;
-            rgb565 |= (g as u16 & 0b11111100) << 3;
-            rgb565 |= (b as u16) >> 3;
-            raw_buffer.extend(rgb565.to_le_bytes());
-        }
-
-        tokio::fs::write(FB_PATH, &raw_buffer).await.unwrap();
-    }
-}
 
 pub fn update_ui(
     task_tracker: &TaskTracker,
@@ -44,7 +19,7 @@ pub fn update_ui(
     generic_framebuffer::update_ui(
         task_tracker,
         config,
-        Framebuffer,
+        FramebufferDevice::new(FB_PATH, None, None),
         ui_shutdown_rx,
         ui_update_rx,
     )
