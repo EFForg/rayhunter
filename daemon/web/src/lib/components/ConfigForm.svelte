e44230c04342c51c49e1605ed66a363bb9785dd1
Cooper Quintin <cooperq@eff.org>
2025-11-04 11:58:32 -0800

    imsi revealing message diagnostic heuristic

--- daemon/web/src/lib/components/ConfigForm.svelte
+++ daemon/web/src/lib/components/ConfigForm.svelte
@@ -335,6 +335,17 @@
                                 Test Heuristic (noisy!)
                             </label>
                         </div>
+                        <div class="flex items-center">
+                            <input
+                                id="imsi_attach_analyzer"
+                                type="checkbox"
+                                bind:checked={config.analyzers.imsi_attach_analyzer}
+                                class="h-4 w-4 text-rayhunter-blue focus:ring-rayhunter-blue border-gray-300 rounded"
+                            />
+                            <label for="imsi_attach_analyzer" class="ml-2 block text-sm text-gray-700">
+                                IMSI Attach Analyzer
+                            </label>
+                        </div>
                     </div>
                 </div>
 
