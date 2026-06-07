49f44e0066aae8bc56f6d1e22fba7f7bfc4d4a77
Jack Lund <jackl@geekheads.net>
2026-06-07 17:32:28 -0500

    WIP

--- lib/src/analysis/wifi_oui_analyzer.rs
+++ lib/src/analysis/wifi_oui_analyzer.rs
@@ -1,4 +1,10 @@
-use log::debug;
+use log::{LevelFilter, Log, debug, info};
+use log4rs::{
+    append::file::FileAppender,
+    config::{Appender, Root},
+    encode::pattern::PatternEncoder,
+    Config, Logger,
+};
 
 use crate::analysis::{
     analyzer::{Analyzer, Event, EventType},
@@ -7,13 +13,29 @@ use crate::analysis::{
 
 pub struct WifiOUIAnalyzer {
     wifi_ouis: Vec<String>,
+    logger: Box<dyn Log>,
 }
 
 impl WifiOUIAnalyzer {
     pub fn new(ouis: &[String]) -> Self {
         Self {
             wifi_ouis: ouis.to_vec(),
+            logger: Box::new(Self::init_logger()),
         }
+    }
+
+    fn init_logger() -> impl Log {
+        let logfile = FileAppender::builder()
+            .encoder(Box::new(PatternEncoder::new("{l} - {m}\n")))
+            .build("/data/rayhunter/wifi.log")
+            .expect("Error creating FileAppender for wifi logs");
+
+        let config = Config::builder()
+            .appender(Appender::builder().build("logfile", Box::new(logfile)))
+            .build(Root::builder().appender("logfile").build(LevelFilter::Info))
+            .expect("Error creating config for wifi logs Logger");
+
+        Logger::new(config)
     }
 }
 
@@ -45,9 +67,10 @@ impl Analyzer for WifiOUIAnalyzer {
                         .find(|oui| bssid.to_uppercase().starts_with(&oui.to_uppercase()))
                         .is_some()
                     {
-                        debug!("Found match for bssid {bssid}");
+                        info!("Logging match to wifi.log");
+                        info!(logger: self.logger, "Found match for bssid {bssid}");
                         return Some(Event {
-                            event_type: EventType::High,
+                            event_type: EventType::Informational,
                             message: "Detected possible IMSI catcher wifi endpoint".to_string(),
                         });
                     }
