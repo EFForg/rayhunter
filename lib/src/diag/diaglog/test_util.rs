a51cafbb14c8b7d6dc7bb2e979720399e7feb8cc
Will Greenberg <willg@eff.org>
2026-06-11 22:06:53 -0700

    lib/diag/diaglog: add MAC parsing for RACH attempts
    This adds a deku parser for MAC RACH packets, along with some unit tests
    adapted from SCAT's parser.

---
+++ lib/src/diag/diaglog/test_util.rs
@@ -0,0 +1,11 @@
+use std::io::Cursor;
+use deku::reader::Reader;
+
+pub fn unhexlify(hexlified_bytes: &str) -> (usize, Reader<Cursor<Vec<u8>>>) {
+    let byte_len = hexlified_bytes.len() / 2;
+    let bytes = (0..hexlified_bytes.len())
+        .step_by(2)
+        .map(|i| u8::from_str_radix(&hexlified_bytes[i..i+2], 16).unwrap())
+        .collect();
+    (byte_len, Reader::new(Cursor::new(bytes)))
+}
