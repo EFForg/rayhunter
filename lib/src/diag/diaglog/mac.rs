759b2ea4c5b24b5c088bd792c70aa74acfa947d9
Will Greenberg <willg@eff.org>
2026-06-11 22:11:04 -0700

    run cargo fmt

--- lib/src/diag/diaglog/mac.rs
+++ lib/src/diag/diaglog/mac.rs
@@ -26,8 +26,8 @@ pub enum SubpacketBody {
     #[deku(id_pat = "_")]
     Other {
         #[deku(count = "size")]
-        data: Vec<u8>
-    }
+        data: Vec<u8>,
+    },
 }
 
 pub mod rach {
@@ -78,7 +78,7 @@ pub mod rach {
         pub p_max: u8,
         pub scell_id: u8,
         pub unk1: u32,
-        pub unk2: u32
+        pub unk2: u32,
     }
 
     #[derive(DekuRead, DekuWrite, Debug, Clone, PartialEq)]
@@ -103,7 +103,7 @@ pub mod rach {
             preamble_power_offset: i16,
             unk1: u16,
             group: i8,
-        }
+        },
     }
 
     #[derive(DekuRead, DekuWrite, Debug, Clone, PartialEq)]
@@ -140,7 +140,7 @@ pub mod rach {
             rach_result: u8,
             contention: u8,
             msg_bitmask: u8,
-        }
+        },
     }
 
     impl AttemptHeader {
@@ -167,9 +167,9 @@ pub mod rach {
 
 #[cfg(test)]
 mod test {
-    use crate::diag::diaglog::mac::rach::{AdditionalInfo, AttemptHeader, Msg1, Msg2, Msg3};
-    use super::*;
     use super::super::test_util::unhexlify;
+    use super::*;
+    use crate::diag::diaglog::mac::rach::{AdditionalInfo, AttemptHeader, Msg1, Msg2, Msg3};
 
     use std::io::Seek;
 
@@ -217,56 +217,198 @@ mod test {
          */
         assert_rach_subpacket(
             "0101a06906022400010001071BFF98FF000001231A0400181C010007000600465C80BD0648000000",
-            rach::AttemptHeader::V2 { num_attempt: 1, rach_result: 0, contention: 1, msg_bitmask: 7 },
-            Some(Msg1::V2 { preamble_index: 27, preamble_index_mask: 255, preamble_power_offset: -104 }),
-            Some(Msg2 { backoff: 0, result: 1, tc_rnti: 6691, ta: 4 }),
-            Some(Msg3 { grant_raw: 72728, grant: 7, harq_id: 6, mac_pdu: [0x00, 0x46, 0x5c, 0x80, 0xbd, 0x06, 0x48, 0x00, 0x00, 0x00] }),
+            rach::AttemptHeader::V2 {
+                num_attempt: 1,
+                rach_result: 0,
+                contention: 1,
+                msg_bitmask: 7,
+            },
+            Some(Msg1::V2 {
+                preamble_index: 27,
+                preamble_index_mask: 255,
+                preamble_power_offset: -104,
+            }),
+            Some(Msg2 {
+                backoff: 0,
+                result: 1,
+                tc_rnti: 6691,
+                ta: 4,
+            }),
+            Some(Msg3 {
+                grant_raw: 72728,
+                grant: 7,
+                harq_id: 6,
+                mac_pdu: [0x00, 0x46, 0x5c, 0x80, 0xbd, 0x06, 0x48, 0x00, 0x00, 0x00],
+            }),
             None,
         );
 
         assert_rach_subpacket(
             "0101a0690603280001000100010718ffa4ff000001c6610b00b4a2000012000120061f423f8d95075800",
-            rach::AttemptHeader::V3 { sub_id: 1, cell_id: 0, num_attempt: 1, rach_result: 0, contention: 1, msg_bitmask: 7 },
-            Some(Msg1::V3Or31 { preamble_index: 24, preamble_index_mask: 255, preamble_power_offset: -92 }),
-            Some(Msg2 { backoff: 0, result: 1, tc_rnti: 25030, ta: 11 }),
-            Some(Msg3 { grant_raw: 41652, grant: 18, harq_id: 1, mac_pdu: [0x20, 0x06, 0x1f, 0x42, 0x3f, 0x8d, 0x95, 0x07, 0x58, 0x00] }),
+            rach::AttemptHeader::V3 {
+                sub_id: 1,
+                cell_id: 0,
+                num_attempt: 1,
+                rach_result: 0,
+                contention: 1,
+                msg_bitmask: 7,
+            },
+            Some(Msg1::V3Or31 {
+                preamble_index: 24,
+                preamble_index_mask: 255,
+                preamble_power_offset: -92,
+            }),
+            Some(Msg2 {
+                backoff: 0,
+                result: 1,
+                tc_rnti: 25030,
+                ta: 11,
+            }),
+            Some(Msg3 {
+                grant_raw: 41652,
+                grant: 18,
+                harq_id: 1,
+                mac_pdu: [0x20, 0x06, 0x1f, 0x42, 0x3f, 0x8d, 0x95, 0x07, 0x58, 0x00],
+            }),
             None,
         );
 
         assert_rach_subpacket(
             "0101739e063134000100010000033f0098ff0000013c6b070058ac010007000000468f47e2d446000000644b0000180001000000d5040000",
-            rach::AttemptHeader::V3 { sub_id: 1, cell_id: 0, num_attempt: 1, rach_result: 0, contention: 0, msg_bitmask: 3 },
-            Some(Msg1::V3Or31 { preamble_index: 63, preamble_index_mask: 0, preamble_power_offset: -104 }),
-            Some(Msg2 { backoff: 0, result: 1, tc_rnti: 27452, ta: 7 }),
+            rach::AttemptHeader::V3 {
+                sub_id: 1,
+                cell_id: 0,
+                num_attempt: 1,
+                rach_result: 0,
+                contention: 0,
+                msg_bitmask: 3,
+            },
+            Some(Msg1::V3Or31 {
+                preamble_index: 63,
+                preamble_index_mask: 0,
+                preamble_power_offset: -104,
+            }),
+            Some(Msg2 {
+                backoff: 0,
+                result: 1,
+                tc_rnti: 27452,
+                ta: 7,
+            }),
             None,
-            Some(AdditionalInfo { ul_earfcn: 19300, p_max: 24, scell_id: 0, unk1: 1, unk2: 1237 }),
+            Some(AdditionalInfo {
+                ul_earfcn: 19300,
+                p_max: 24,
+                scell_id: 0,
+                unk1: 1,
+                unk2: 1237,
+            }),
         );
 
-       assert_rach_subpacket(
+        assert_rach_subpacket(
             "01010000063134000100010001070aff98ff0000011c48070018e2000007000000523b7dfd69b6000000f5540000ff0001000000d6040000",
-            AttemptHeader::V3 { sub_id: 1, cell_id: 0, num_attempt: 1, rach_result: 0, contention: 1, msg_bitmask: 7 },
-            Some(Msg1::V3Or31 { preamble_index: 10, preamble_index_mask: 255, preamble_power_offset: -104 }),
-            Some(Msg2 { backoff: 0, result: 1, tc_rnti: 18460, ta: 7 }),
-            Some(Msg3 { grant_raw: 57880, grant: 7, harq_id: 0, mac_pdu: [0x00, 0x52, 0x3b, 0x7d, 0xfd, 0x69, 0xb6, 0x00, 0x00, 0x00] }),
-            Some(AdditionalInfo { ul_earfcn: 21749, p_max: 255, scell_id: 0, unk1: 1, unk2: 1238 }),
+            AttemptHeader::V3 {
+                sub_id: 1,
+                cell_id: 0,
+                num_attempt: 1,
+                rach_result: 0,
+                contention: 1,
+                msg_bitmask: 7,
+            },
+            Some(Msg1::V3Or31 {
+                preamble_index: 10,
+                preamble_index_mask: 255,
+                preamble_power_offset: -104,
+            }),
+            Some(Msg2 {
+                backoff: 0,
+                result: 1,
+                tc_rnti: 18460,
+                ta: 7,
+            }),
+            Some(Msg3 {
+                grant_raw: 57880,
+                grant: 7,
+                harq_id: 0,
+                mac_pdu: [0x00, 0x52, 0x3b, 0x7d, 0xfd, 0x69, 0xb6, 0x00, 0x00, 0x00],
+            }),
+            Some(AdditionalInfo {
+                ul_earfcn: 21749,
+                p_max: 255,
+                scell_id: 0,
+                unk1: 1,
+                unk2: 1238,
+            }),
         );
 
         assert_rach_subpacket(
             "01010000063238000100010000032900a4ffeb000000000195b603000000a0b412000420061f425dc9be41b800885e000017000100000065050000",
-            AttemptHeader::V3 { sub_id: 1, cell_id: 0, num_attempt: 1, rach_result: 0, contention: 0, msg_bitmask: 3 },
-            Some(Msg1::V32 { preamble_index: 41, preamble_index_mask: 0, preamble_power_offset: -92, unk1: 235, group: 0 }),
-            Some(Msg2 { backoff: 0, result: 1, tc_rnti: 46741, ta: 3 }),
+            AttemptHeader::V3 {
+                sub_id: 1,
+                cell_id: 0,
+                num_attempt: 1,
+                rach_result: 0,
+                contention: 0,
+                msg_bitmask: 3,
+            },
+            Some(Msg1::V32 {
+                preamble_index: 41,
+                preamble_index_mask: 0,
+                preamble_power_offset: -92,
+                unk1: 235,
+                group: 0,
+            }),
+            Some(Msg2 {
+                backoff: 0,
+                result: 1,
+                tc_rnti: 46741,
+                ta: 3,
+            }),
             None,
-            Some(AdditionalInfo { ul_earfcn: 24200, p_max: 23, scell_id: 0, unk1: 1, unk2: 1381 }),
+            Some(AdditionalInfo {
+                ul_earfcn: 24200,
+                p_max: 23,
+                scell_id: 0,
+                unk1: 1,
+                unk2: 1381,
+            }),
         );
 
         assert_rach_subpacket(
             "010100000632380001000100010713ffa0ffeb0000000001ad5a0500000146b412000420061f425dc9be41b400665300001800010000001a050000",
-            AttemptHeader::V3 { sub_id: 1, cell_id: 0, num_attempt: 1, rach_result: 0, contention: 1, msg_bitmask: 7 },
-            Some(Msg1::V32 { preamble_index: 19, preamble_index_mask: 255, preamble_power_offset: -96, unk1: 235, group: 0 }),
-            Some(Msg2 { backoff: 0, result: 1, tc_rnti: 23213, ta: 5 }),
-            Some(Msg3 { grant_raw: 3024486656, grant: 18, harq_id: 4, mac_pdu: [0x20, 0x06, 0x1f, 0x42, 0x5d, 0xc9, 0xbe, 0x41, 0xb4, 0x00] }),
-            Some(AdditionalInfo { ul_earfcn: 21350, p_max: 24, scell_id: 0, unk1: 1, unk2: 1306 }),
+            AttemptHeader::V3 {
+                sub_id: 1,
+                cell_id: 0,
+                num_attempt: 1,
+                rach_result: 0,
+                contention: 1,
+                msg_bitmask: 7,
+            },
+            Some(Msg1::V32 {
+                preamble_index: 19,
+                preamble_index_mask: 255,
+                preamble_power_offset: -96,
+                unk1: 235,
+                group: 0,
+            }),
+            Some(Msg2 {
+                backoff: 0,
+                result: 1,
+                tc_rnti: 23213,
+                ta: 5,
+            }),
+            Some(Msg3 {
+                grant_raw: 3024486656,
+                grant: 18,
+                harq_id: 4,
+                mac_pdu: [0x20, 0x06, 0x1f, 0x42, 0x5d, 0xc9, 0xbe, 0x41, 0xb4, 0x00],
+            }),
+            Some(AdditionalInfo {
+                ul_earfcn: 21350,
+                p_max: 24,
+                scell_id: 0,
+                unk1: 1,
+                unk2: 1306,
+            }),
         );
     }
 }
--- lib/src/diag/diaglog/test_util.rs
+++ lib/src/diag/diaglog/test_util.rs
@@ -1,11 +1,11 @@
-use std::io::Cursor;
 use deku::reader::Reader;
+use std::io::Cursor;
 
 pub fn unhexlify(hexlified_bytes: &str) -> (usize, Reader<Cursor<Vec<u8>>>) {
     let byte_len = hexlified_bytes.len() / 2;
     let bytes = (0..hexlified_bytes.len())
         .step_by(2)
-        .map(|i| u8::from_str_radix(&hexlified_bytes[i..i+2], 16).unwrap())
+        .map(|i| u8::from_str_radix(&hexlified_bytes[i..i + 2], 16).unwrap())
         .collect();
     (byte_len, Reader::new(Cursor::new(bytes)))
 }
