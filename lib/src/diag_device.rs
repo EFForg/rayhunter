967e8238057e738feb31793e8dd871a81305d86e
Will Greenberg <willg@eff.org>
2026-06-24 15:52:08 -0700

    rm unimplemented diag message format

--- lib/src/diag_device.rs
+++ lib/src/diag_device.rs
@@ -40,7 +40,7 @@ pub enum DiagDeviceError {
     ParseMessagesContainerError(deku::DekuError),
 }
 
-pub const LOG_CODES_FOR_RAW_PACKET_LOGGING: [u32; 17] = [
+pub const LOG_CODES_FOR_RAW_PACKET_LOGGING: [u32; 16] = [
     // Layer 2:
     log_codes::LOG_GPRS_MAC_SIGNALLING_MESSAGE_C, // 0x5226
     // Layer 3:
@@ -58,7 +58,6 @@ pub const LOG_CODES_FOR_RAW_PACKET_LOGGING: [u32; 17] = [
     log_codes::LOG_DATA_PROTOCOL_LOGGING_C, // 0x11eb
     // LTE physical layer serving cell measurements: RSRP, RSRQ, RSSI
     log_codes::LOG_LTE_ML1_SERVING_CELL_MEAS_AND_EVAL_C, // 0xb17f
-    log_codes::LOG_LTE_ML1_SERVING_CELL_MEAS_RESPONSE,   // 0xb193
     log_codes::LOG_LTE_ML1_NEIGHBOR_MEAS,                // 0xb180
     // LTE MAC Random Access Channel response: contains Timing Advance
     log_codes::LOG_LTE_MAC_RACH_RESPONSE_C, // 0xb062
