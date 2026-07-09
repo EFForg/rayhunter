075ae08aced0ad3b23b495904de8c93b1fedf67a
Will Greenberg <willg@eff.org>
2026-07-08 17:37:12 -0700

    run cargo fmt

--- lib/src/diag/diaglog/ll1.rs
+++ lib/src/diag/diaglog/ll1.rs
@@ -9,9 +9,17 @@ pub struct ServingCellTiming {
     pub num_records: u8,
     #[deku(bits = 4, assert = "*starting_sub_fn <= 9")]
     pub starting_sub_fn: u8,
-    #[deku(bits = 10, pad_bits_after = "5", assert = "*starting_system_fn <= 1023")]
+    #[deku(
+        bits = 10,
+        pad_bits_after = "5",
+        assert = "*starting_system_fn <= 1023"
+    )]
     pub starting_system_fn: u16,
-    #[deku(bits = 19, pad_bits_after = "13", assert = "*starting_dl_frame_timing_offs <= 307200")]
+    #[deku(
+        bits = 19,
+        pad_bits_after = "13",
+        assert = "*starting_dl_frame_timing_offs <= 307200"
+    )]
     pub starting_dl_frame_timing_offs: u32, // in Ts units
     #[deku(bits = 19, assert = "*starting_ul_frame_timing_offs <= 307200")]
     pub starting_ul_frame_timing_offs: u32, // in Ts units
@@ -24,7 +32,10 @@ pub struct ServingCellTiming {
 #[derive(Debug, Clone, PartialEq, DekuRead, DekuWrite)]
 #[deku(bit_order = "lsb", ctx = "_: deku::ctx::Order")]
 pub struct TimingAdjustment {
-    #[deku(bits = 11, assert = "(-512..=511).contains(dl_frame_timing_adjustment)")]
+    #[deku(
+        bits = 11,
+        assert = "(-512..=511).contains(dl_frame_timing_adjustment)"
+    )]
     pub dl_frame_timing_adjustment: i16, // in Ts units
     #[deku(bits = 5, assert = "(-16..=15).contains(ul_frame_timing_adjustment)")]
     pub ul_frame_timing_adjustment: i8, // in Ts units
