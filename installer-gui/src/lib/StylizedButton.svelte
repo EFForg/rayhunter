075bb4c7c7a2c53ba7464269f0e3f5cc0c5e81c6
Brad Warren <bmw@eff.org>
2026-08-20 11:41:00 -0700

    add InstallProgress screen

---
+++ installer-gui/src/lib/StylizedButton.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    let {
+        color = 'white',
+        disabled = false,
+        label,
+        onclick,
+    }: {
+        color?: 'blue' | 'white';
+        disabled?: boolean;
+        label: string;
+        onclick: () => void;
+    } = $props();
+</script>
+
+<button
+    class="
+        {color === 'blue' ? 'bg-rayhunter-blue text-white' : ''}
+        {disabled ? '' : 'cursor-pointer'} rayhunter-button"
+    {disabled}
+    {onclick}
+>
+    {label}
+</button>
