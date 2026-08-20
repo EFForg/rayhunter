a3a365a98a4bd6e1d324c755db6adc1ab92ed9f3
Brad Warren <bmw@eff.org>
2026-08-20 11:41:00 -0700

    add InstallProgress screen

---
+++ installer-gui/src/lib/InstallProgress.svelte
@@ -0,0 +1,97 @@
+<script lang="ts">
+    import { onDestroy, tick } from 'svelte';
+    import { invoke } from '@tauri-apps/api/core';
+    import { emit, listen } from '@tauri-apps/api/event';
+    import { exit } from '@tauri-apps/plugin-process';
+
+    import StylizedButton from '$lib/StylizedButton.svelte';
+
+    type InstallerState = 'Running' | 'Succeeded' | 'Failed';
+
+    let {
+        installerArgs,
+        reselect_args,
+    }: {
+        installerArgs: string;
+        reselect_args: () => void;
+    } = $props();
+
+    let currentState: InstallerState = $state('Running');
+    let installerOutput = $state('');
+    let outputDiv: HTMLDivElement | undefined;
+
+    const listenPromise = listen<string>('installer-output', (event) => {
+        // The number in the comparison specifies the number of pixels the
+        // scrollbar needs to be from the bottom to autoscroll as new text is
+        // added. The current value was chosen somewhat arbitrarily.
+        const autoscroll =
+            outputDiv && outputDiv.scrollHeight - outputDiv.scrollTop - outputDiv.clientHeight < 25;
+
+        installerOutput += event.payload;
+
+        if (autoscroll) {
+            tick().then(() => {
+                if (outputDiv) {
+                    outputDiv.scrollTop = outputDiv.scrollHeight;
+                }
+            });
+        }
+    });
+
+    onDestroy(async () => {
+        const unlisten = await listenPromise;
+        unlisten();
+    });
+
+    async function run_installer() {
+        // We await to ensure the listener is set up before continuing.
+        await listenPromise;
+
+        try {
+            await invoke('install_rayhunter', { args: installerArgs });
+            currentState = 'Succeeded';
+        } catch (error) {
+            // Just to be safe and ensure correct output order, we use emit
+            // rather than modifying installerOutput directly.
+            emit('installer-output', error);
+            currentState = 'Failed';
+        }
+    }
+
+    run_installer();
+</script>
+
+<div class="mx-8">
+    <h1 class="font-semibold mb-4 text-center text-2xl">
+        {#if currentState === 'Running'}
+            Installing Rayhunter
+        {:else if currentState === 'Succeeded'}
+            Installation succeeded!
+        {:else}
+            Installation failed!
+        {/if}
+    </h1>
+    <div
+        bind:this={outputDiv}
+        class="bg-gray-800 max-h-[60vh] 2xl:max-h-[70vh] overflow-y-auto p-4 rounded-xl text-gray-200 whitespace-pre-line"
+    >
+        {installerOutput}
+    </div>
+    {#if currentState !== 'Running'}
+        <div class="flex justify-evenly mt-6">
+            {#if currentState === 'Succeeded'}
+                <StylizedButton label="Install another device" onclick={reselect_args} />
+                <StylizedButton color="blue" label="Exit" onclick={() => exit(0)} />
+            {:else}
+                <StylizedButton color="blue" label="Retry" onclick={reselect_args} />
+                <a
+                    class="rayhunter-button"
+                    href="https://github.com/EFForg/rayhunter/issues"
+                    target="_blank"
+                >
+                    Report Issue
+                </a>
+            {/if}
+        </div>
+    {/if}
+</div>
