c1a83d5bbbfe8f813ddb7e7daac4cf4872e10f58
Brad Warren <bmw@eff.org>
2026-08-20 11:22:08 -0700

    run `npm run tauri add process`

--- installer-gui/src-tauri/src/lib.rs
+++ installer-gui/src-tauri/src/lib.rs
@@ -41,6 +41,7 @@ fn rayhunter_options() -> introspect::Command<'static> {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![install_rayhunter])
         .invoke_handler(tauri::generate_handler![rayhunter_options])
